b83a5114b74402be21e31c1020a4de5c3a36fb67
songdo3303 <songdo3303@gmail.com>
2026-05-02 15:34:34 +0900

    Update submit.js

--- api/submit.js
+++ api/submit.js
@@ -1,7 +1,7 @@
 const { Resend } = require('resend');
 
 const resend = new Resend(process.env.RESEND_API_KEY);
-const TO_EMAIL = 'sksk3222@naver.com';
+const TO_EMAIL = 'songdo3303@gmail.com';
 const TO_PHONE = '010-7548-3222';
 
 module.exports = async (req, res) => {
